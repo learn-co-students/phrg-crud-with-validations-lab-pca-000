c23daff944ff94a5a2b7d43a07390007907adaf8
KFad <kfadigan@yahoo.com>
2019-05-19 19:49:59 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: "The artist can't put out the same \
+    song in the same year... c'mon...",
+  }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year,
+    }
+  end
+
+  def released?
+    released
+  end
+end
